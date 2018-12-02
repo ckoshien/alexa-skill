f77d8b4f1b3df065efe6000eac9f28c8f789d756
ckoshien <ckoshien@gmail.com>
2018-12-02 14:41:11 +0900

    no message

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,7 +10,16 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
   console.log(req)
   //res.render('index', { title: 'Express' });
-  res.json(200, { title: 'Express' })
+  res.json(200,
+    {response:{
+      "outputSpeech": {
+        "type": "PlainText",
+        "text": "こんにちは、こちらはサーバプログラムです。"
+      }
+    }
+  }
+  )
+  return;
 });
 
 module.exports = router;
