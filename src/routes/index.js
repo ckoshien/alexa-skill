195715ebbf826eee5ab4ff50331dd8a00c850481
ckoshien <ckoshien@gmail.com>
2018-12-02 15:31:11 +0900

    node-fetchインストール リマインダーAPIへのアクセス追加

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fetch = require('node-fetch')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,23 +9,55 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  console.log(req)
+  //console.log(req.body.context.System.apiAccessToken)
   //res.render('index', { title: 'Express' });
-  //{
-    return handlerInput.responseBuilder
-        .speak('ようこそ、コーヒーショップへ、ご注文をどうぞ')
-        .reprompt('ご注文をお伺いします')
-        .getResponse();
-  //}
-  // res.json(200,
-  //   {
-  //     response:{
-  //     "outputSpeech": {
-  //       "type": "PlainText",
-  //       "text": "こんにちは、こちらはサーバプログラムです。"
-  //     }
-  //   }
-    //})
+  const method = 'POST'
+  const headers= {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer '+req.body.context.System.apiAccessToken,
+    'Content-length': 200
+  }
+  const body={
+    "requestTime" : "2018-12-03T19:04:00.672",
+    "trigger": {
+         "type" : "SCHEDULED_ABSOLUTE",                 // トリガーのタイプを示します
+         "scheduledTime" : "2018-12-03T19:00:00.000",    // 有効なISO 8601形式 - 想定されるトリガー時刻
+         "timeZoneId" : "America/Los_Angeles",           // タイムゾーンのIDを含む文字列。 以下の「タイムゾーンの動作」セクションを参照してください。
+         "recurrence" : {                               // 反復情報をすべて含める必要があります
+             "freq" : "WEEKLY",                         // 反復の頻度のタイプ
+             "byDay": ["MO"],                           // 一週間の中での日のリスト
+         }
+    },
+    "alertInfo": {
+         "spokenInfo": {
+             "content": [{
+                 "locale": "en-US",                     // 値が指定されるロケール
+                 "text": "犬の散歩"                // 表示および読み上げ用に使用されるテキスト
+             }]
+         }
+     },
+     "pushNotification" : {                            
+          "status" : "ENABLED"                          // プッシュ通知を送信するかどうか [default = ENABLED]
+     }
+ }
+  fetch('https://api.amazonalexa.com/v1/alerts/reminders',
+  {method,headers,body})
+  .then((response) => {
+    return response.json()
+  })
+  .then((json)=>{
+    console.log(json)
+  })
+  res.json(200,
+    {
+      response:{
+      "outputSpeech": {
+        "type": "PlainText",
+        "text": "こんにちは、こちらはサーバプログラムです。"
+      }
+    }
+  }
+  )
   //return;
 });
 
