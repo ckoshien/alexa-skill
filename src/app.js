98709009cb8b8bd84cd5ca6b8e14fe2b0f5c827c
ckoshien <ckoshien@gmail.com>
2018-12-14 13:01:31 +0900

    app.useをコメントアウト https://github.com/alexa-js/alexa-app/issues/49#issuecomment-270008059

--- src/app.js
+++ src/app.js
@@ -9,21 +9,21 @@ const alexa = require('./routes/alexa');
 //var usersRouter = require('./routes/users');
 
 var app = express();
+alexa.express({
+  expressApp: app,
+  endpoint: '/alexa',
+  checkCert: true
+});
 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser());
-alexa.express({
-  expressApp: app,
-  endpoint: '/alexa',
-  //checkCert: true
-});
-app.use(express.static(path.join(__dirname, 'build')));
+//app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   //console.log(req.originalUrl)
   res.sendFile(path.join(__dirname,'..', 'build','index.html'));
