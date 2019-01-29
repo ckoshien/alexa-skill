759f456e116fb435bed4213cf3e54d0b7e267f4c
ckoshien <ckoshien@gmail.com>
2019-01-29 21:27:51 +0900

    no message

--- src/routes/alexa.js
+++ src/routes/alexa.js
@@ -43,7 +43,9 @@ const averageResponse = (req, res) => {
     console.log(res)
     //var json = JSON.parse(data)
     if(num <= 10){
-      res.say(num+"位<break time='100ms'/>"+json['averageTop10'][num - 1]['name']+':'+json['averageTop10'][num - 1]['average']).shouldEndSession(true)
+      res.say(num+"位<break time='100ms'/")
+      res.say(json['averageTop10'][num - 1]['name'])
+      res.say(json['averageTop10'][num - 1]['average']).shouldEndSession(true)
     }else{
       res.say("ごめんなさい。<break time='100ms'/>"+num+"は指定できません。<break time='100ms'/>10以下の順位を指定してください").shouldEndSession(false);
     }  
