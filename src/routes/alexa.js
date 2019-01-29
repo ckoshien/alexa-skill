17585f0b2b849d71249f986860cb892535978cb8
ckoshien <ckoshien@gmail.com>
2019-01-29 22:05:46 +0900

    no message

--- src/routes/alexa.js
+++ src/routes/alexa.js
@@ -46,14 +46,19 @@ const averageResponse = (req, res) => {
     console.log(json['averageTop10'][num - 1])
     //console.log(res)
     //var json = JSON.parse(data)
-    if(num <= 10){
-      //res.say(num+"位<break time='100ms'/")
-      //res.say(json['averageTop10'][num - 1]['name'])
-      console.log(json.averageTop10[num - 1].average)
-      res.say(json.averageTop10[num - 1].average.toString()).shouldEndSession(true)
-    }else{
-      res.say("ごめんなさい。<break time='100ms'/>"+num+"は指定できません。<break time='100ms'/>10以下の順位を指定してください").shouldEndSession(false);
-    }  
+    try {
+      if(num <= 10){
+        //res.say(num+"位<break time='100ms'/")
+        //res.say(json['averageTop10'][num - 1]['name'])
+        console.log(json.averageTop10[num - 1].average)
+        res.say(json.averageTop10[num - 1].average.toString()).shouldEndSession(true)
+      }else{
+        res.say("ごめんなさい。<break time='100ms'/>"+num+"は指定できません。<break time='100ms'/>10以下の順位を指定してください").shouldEndSession(false);
+      }  
+        
+    } catch (error) {
+      console.error(error)      
+    }
   })
   .catch((err)=>{
     console.error(err);
