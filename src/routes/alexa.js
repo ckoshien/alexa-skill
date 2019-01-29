32eb49863d4406d82013b4c4daa7d48fbd9d3f31
ckoshien <ckoshien@gmail.com>
2019-01-29 11:29:47 +0900

    no message

--- src/routes/alexa.js
+++ src/routes/alexa.js
@@ -34,15 +34,15 @@ const averageResponse = (req, res) => {
     var num = req.slots['num'].value
     var response = await fetch('https://jcbl.mydns.jp/api/v2/result/season/41')
     var json = await response.json()
-    console.log(num)
-    console.log(json['averageTop10'][num - 1])
+  })();
+  console.log(num)
+  console.log(json['averageTop10'][num - 1])
     //var json = JSON.parse(data)
     if(num <= 10){
       res.say(num+"位<break time='100ms'/>"+json['averageTop10'][num - 1]['name']+':'+json['averageTop10'][num - 1]['average']).shouldEndSession(true)
     }else{
       res.say("ごめんなさい。<break time='100ms'/>"+num+"は指定できません。<break time='100ms'/>10以下の順位を指定してください").shouldEndSession(false);
     }
-  })();
   //var data = fs.readFileSync('data.json')
   
 }
